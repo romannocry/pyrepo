52a60980a065a7e6975c03d4d95289137d0038d5
romannocry <roman.medioni@gmail.com>
2024-01-27 15:40:52 -0500

    changes

--- Archive/lists.py
+++
@@ -1,7 +0,0 @@
-fruitList = ['apple', 'red apple', 'banana','green apple', 'melon']
-
-#appleList = ['apple' in fruit for fruit in fruitList]
-appleList = [x for x in fruitList if 'apple' in x]
-#appleList = [x*2 for x in fruitList]
-
-print(appleList)
