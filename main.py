3e7b002de3d61cd74fc7099b406861c3a1cf8ad1
romannocry <roman.medioni@gmail.com>
2024-01-27 15:44:41 -0500

    changes

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 print("initial commit on main")
 print("this is a commit on UAT")
 
+
 def uat_func2():
     print("to")
-    
+
+
 def uat_func():
     print("func")
 
+
 def uat_john_func():
     print("this is a new uat func")
+
+
 def dev_roman_func():
     print("func")
-
 
 
 def setupaction():
--- utils/typing.py
+++ utils/typing.py
@@ -1,23 +1,27 @@
 from functools import wraps
 
+
 def type_check_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         # Check the types of arguments based on type hints
         for arg, hint in zip(args, func.__annotations__.values()):
             if not isinstance(arg, hint):
-                raise TypeError(f"Argument {arg} should be of type {hint.__name__}")
+                raise TypeError(
+                    f"Argument {arg} should be of type {hint.__name__}")
 
         # Check the types of keyword arguments based on type hints
         for key, value in kwargs.items():
             hint = func.__annotations__.get(key)
             if hint and not isinstance(value, hint):
-                raise TypeError(f"Argument {key} should be of type {hint.__name__}")
+                raise TypeError(
+                    f"Argument {key} should be of type {hint.__name__}")
 
         # If type checks pass, call the original function
         return func(*args, **kwargs)
 
     return wrapper
+
 
 # Specify functions to be exported when using `from decorators import *`
 __all__ = ['type_check_decorator']
