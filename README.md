fd2e6e7b1eb752f3489059485a4a917a66edd528
romannocry <roman.medioni@gmail.com>
2024-01-28 22:32:26 -0500

    data and function

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-README
+Synthetic function for reporting
+1) connects to an API (only one api call to start)
+    - input client id
+    - input client secret
+    - input api uri
+    - input scopes
+2) email processing & templating
+    - conditional filtering: applies filtering on the dataset retrieved
+    - data columns you need
+    - data enrichment: matchB
+    - recipients matrix: generate email jobs for email + column(s)/value pairs
+    - default recipients: generate email job without applying any filtering
+    - templating
+        - output: data table, pandas summarized dataset
--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-print("initial commit on main")
-print("this is a commit on UAT")
+import pandas as pd
+from utils.typing import *
 
 
-def uat_func2():
-    print("to")
+columns_selection = ['Survived']
+filtering_rules = [('Survived',0)]
+recipients = ['x']
+recipients_matrix = [{'email':'x@x.com','col1:':'value','col2':'value2'},{'email':'y@y.com','col1:':'value','col2':'value2'}]
+
+def generate_statistics(df: pd.DataFrame):
+    """
+    describe function from pandas
+    """
+    return df.describe()
+
+def apply_filtering(df:pd.DataFrame, filtering_rules: list):
+    """
+    #1 Applies first layer of filtering
+    """
+    df = df
+    for key, value in filtering_rules:
+        print(f"filtering data on Key: {key} and value: {value}")
+        df = df[df[key] == value]
+    
+    return df
+
+def apply_column_selection(columns: list):
+    """
+    Filters the columns that needs to be kept
+    if empty - no filtering
+    """
+
+def generate_emails(recipients: list, recipients_matrix: list):
+    """
+    generate email notifications: 
+    """
+
+def main():
+    data = pd.read_csv("./utils/fake_data.csv")
+    data_filtered = apply_filtering(data,filtering_rules)
+
+    stats = generate_statistics(data)
+    print(stats)
+
+    stats2 = generate_statistics(data_filtered)
+    print(stats2)
 
 
-def uat_func():
-    print("func")
-
-
-def uat_john_func():
-    print("this is a new uat func")
-
-
-def dev_roman_func():
-    print("func")
-
-
-def setupaction():
-    print(f'to')
+if __name__ == "__main__":
+    main()
