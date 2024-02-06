9b4cbca8f1ab82502215d7068e21deb210ff876c
romannocry <roman.medioni@gmail.com>
2024-02-05 21:31:27 -0500

    change some logic

--- function/main.py
+++ function/main.py
@@ -125,7 +125,7 @@ def apply_enrichment(df:pd.DataFrame, data_enrichment_json_string:str):
         print(f"Error during merge: {e}")
     return df
 
-def generate_template(dfs: [pd.DataFrame]):
+def generate_template_old(dfs: [pd.DataFrame]):
     # Jinja2 template
     template_str = """
     <html>
@@ -168,6 +168,7 @@ def generate_template(dfs: [pd.DataFrame]):
     for df in dfs:
         table_data.append((df.columns.tolist(),df.to_dict(orient='records')))
 
+
     template = Template(template_str)
     testlist = [1,2]
     # Example data
@@ -184,63 +185,128 @@ def generate_template(dfs: [pd.DataFrame]):
     rendered_html = template.render(**data)
     return rendered_html
 
-def generate_emails(recipients: list, recipients_matrix: list, df:pd.DataFrame):
+
+def generate_template(dfs: [pd.DataFrame], css: str):
+    # Jinja2 template
+    template_str = """
+    <html>
+    <head>
+        {{css}}
+        <title>{{ title }}</title>
+    </head>
+    <body>
+        <h1>Hello, {{ name }}!</h1>
+        <p>{{ message }}</p>
+        {% for table in table_data %}
+            {{ table }}
+        </br></br>
+        {% endfor %}
+    </body>
+    </html>
+    """
+    table_data = []
+    for df in dfs:
+        # Convert DataFrame to HTML
+        html_table = df.to_html(index=True)
+        table_data.append(html_table)
+
+
+    template = Template(template_str)
+    testlist = [1,2]
+    # Example data
+    data = {
+        'css': css,
+        'title': 'My Email',
+        'name': 'John',
+        'message': 'Welcome to the community!',
+        'table_data': table_data,
+    }
+
+    rendered_html = template.render(**data)
+    return rendered_html
+
+
+def generate_emails(recipients: list, recipients_matrix: list, df:pd.DataFrame, include_counts: bool, include_dataset_description: bool):
     """
     generate email notifications:
     #1 all recipients receive the full dataframe
     #2 all matrix recipents receive a dataframe that goes through an additionnal layer of filtering
     """
+    #lower case required for filtering subsets
+    recipients_matrix = recipients_matrix.lower()
+
+    dataframes_recipients = []
+    dataframes_recipients.append(df)
+    if include_counts: dataframes_recipients.append(generate_dataset_count_stats(df, True))
+    if include_dataset_description: dataframes_recipients.append(generate_dataset_description(df))
     for recipient in recipients:
-        send_email(recipient,df)
+        send_email(recipient,dataframes_recipients)
     
+    dataframes_recipients_matrix = []
     # Convert JSON string to a Python dictionary
     recipients_matrix = json.loads(recipients_matrix)
     for recipient in recipients_matrix:
             filtering_tuples = [tuple(inner_list) for inner_list in recipient['filtering_rules']]
-            print(filtering_tuples)
             df = apply_row_filtering(df,filtering_tuples)
-            send_email(recipient['email'], df)
+            dataframes_recipients_matrix.append(df)
+            if include_counts: dataframes_recipients_matrix.append(generate_dataset_count_stats(df, True))
+            if include_dataset_description: dataframes_recipients_matrix.append(generate_dataset_description(df))
+            send_email(recipient['email'], dataframes_recipients_matrix)
 
 
-def send_email(recipient:str, df:pd.DataFrame):
+def send_email(recipient:str, dfs:[pd.DataFrame]):
+    template = generate_template(dfs, '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">')
+        # Specify the file path and name
+    file_path = "example.html"
+    # Write the HTML content to the file
+    with open(file_path, "w") as file:
+        file.write(template)
+
+    print(f"HTML file '{file_path}' created successfully.")
     print(f'sending email to {recipient}')
-    #print(df)
 
-def main():
-
-
-  
-    #data manipulation parameters (optional)
-    filtering_rules = [('Age',22),('Sex','female')]
-    columns_selection = ['Name','Survived','Sex','Cabin','Age']
+def pre_process_dataframe(data: pd.DataFrame, filtering_rules: list, columns_selection: list, data_enrichment_json_string:str):
     
-    data_enrichment_json_string = '[[{"Survived":0,"favorite_color":"red"},{"Survived":1,"favorite_color":"blue"}],[{"Sex":"male","Sex_":"boy"}]]'
+    # Convert everything to lowercase
+    data.columns = data.columns.str.lower()
+    filtering_rules = [(key.lower(), value) if isinstance(key, str) else (key, value) for key, value in filtering_rules]
+    columns_selection = [element.lower() for element in columns_selection]
+    data_enrichment_json_string = data_enrichment_json_string.lower()
 
-    #emmail parameters
-    recipients = ['x']
-    recipients_matrix = '[{"email":"roman.medioni","filtering_rules":[["Survived", 1],["Sex","male"],["Cabin","A6"]]}]'
-    from_addr = ""
-    email_title = ""
 
-    #get data 
-    data = get_data(sys.path[0]+'/AI/utils/fake_data.csv','csv') 
-    
     #apply pre-filter on rows (optional)
     data_filtered = apply_row_filtering(data,filtering_rules)
     #apply selection on columns (optional)
     data_filtered = apply_column_selection(data_filtered, columns_selection)
     #apply enrichment (optional)
     data_filtered = apply_enrichment(data_filtered,data_enrichment_json_string)
-    
-    print(data_filtered)
-    generate_emails(recipients,recipients_matrix,data_filtered)
 
-    generate_dataset_count_stats(data_filtered, True)
-    stats2 = generate_dataset_description(data_filtered)
-    print(stats2)
+    return data_filtered
 
-    test_html = generate_template([data_filtered])
-    #print(test_html)
+def main():
+
+    file = sys.path[0]+'/AI/utils/fake_data.csv'
+    #get data 
+    data = get_data(file,'csv') 
+
+    #data pre-processing parameters (optional)
+    filtering_rules = [('Sex','female')]
+    columns_selection = ['Name','Survived','Sex','Cabin','Age']
+    data_enrichment_json_string = '[[{"Survived":0,"favorite_color":"red"},{"Survived":1,"favorite_color":"blue"}],[{"Sex":"male","Sex_":"boy"}]]'
+
+    #email parameters
+    recipients = ['x']
+    recipients_matrix = '[]'
+    #recipients_matrix = '[{"email":"roman.medioni","filtering_rules":[["Survived", 1],["Sex","male"],["Cabin","A6"]]}]'
+    from_addr = ""
+    email_title = ""
+
+    #processed data output
+    processed_data = pre_process_dataframe(data, filtering_rules, columns_selection, data_enrichment_json_string)
+    ##Actions
+    #email generation step
+    generate_emails(recipients, recipients_matrix, processed_data, True, True)
+
 
 
 if __name__ == "__main__":
