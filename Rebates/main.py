1057b0585800e7ab17bbef95569239ba166b8880
romannocry <roman.medioni@gmail.com>
2024-02-25 22:16:50 -0500

    simplified code

--- Rebates/main.py
+++ Rebates/main.py
@@ -7,20 +7,6 @@ import sys
 year = 2024
 month = 2
 
-#applies a 0.1 default rebate
-fixed_rebate_example = {
-  "clientId": "C2",
-  "rebates": [
-    {
-      "type": "default",
-      "rebate_amount_per_contract": 0.1,
-      "exchanges": ["e1", "e2"],
-      "salesman_code": ["s1", "s2"],
-      "accounts": ["a1", "a2"]
-    }
-  ]
-}
-
 
 #applies a 0.1 rebate for all the transactions > 500,000 
 yearly_rebate_example = {
@@ -32,7 +18,8 @@ yearly_rebate_example = {
       "rebate_amount_per_contract": 0.1,
       "exchanges": ["E1", "E2","E3","E4"],
       "salesman_code": ["S1", "S2","S3"],
-      "accounts": ["A1", "A2", "A3"]
+      "accounts": ["A1", "A2", "A3"],
+      "product_name":["P1"]
     },
     {
       "type": "monthly",
@@ -40,7 +27,8 @@ yearly_rebate_example = {
       "rebate_amount_per_contract": 0.01,
       "exchanges": ["E1", "E2","E3","E4"],
       "salesman_code": ["S1", "S2","S3"],
-      "accounts": ["A1", "A2", "A3"]
+      "accounts": ["A1", "A2", "A3"],
+      "product_name":["P1"]
     },
     {
       "type": "default",
@@ -48,7 +36,8 @@ yearly_rebate_example = {
       "rebate_amount_per_contract": 0.001,
       "exchanges": ["E1", "E2","E3","E4"],
       "salesman_code": ["S1", "S2","S3"],
-      "accounts": ["A1", "A2", "A3"]
+      "accounts": ["A1", "A2", "A3"],
+      "product_name":["P1"]
     },   
   ]
 }
@@ -79,22 +68,14 @@ yearly_rebate_example_2 = {
   ]
 }
 
-#applies a 0.1 rebate per contract on a monthly basis for transactions > 5000
-monthly_threshold_example = {
-  "clientId": "C3",
-  "rebates": [
-    {
-      "type": "monthly",
-      "threshold": 5000,
-      "rebate_amount_per_contract": 0.1,
-      "exchanges": ["e1", "e2"],
-      "salesman_code": ["s1", "s2"],
-      "accounts": ["a1", "a2"]
-    }
-  ]
-}
 
-examples = [fixed_rebate_example,monthly_threshold_example,yearly_rebate_example]
+examples = [yearly_rebate_example]
+
+# Columns to exclude from the group by operation
+#exclude_columns = ['B']
+# Selecting columns to include
+#included_columns = [col for col in df.columns if col not in exclude_columns]
+
 
 def apply_yearly_rebate(rebate, client, df):
     # Calculate and apply yearly rebate logic here
@@ -115,7 +96,7 @@ def apply_yearly_rebate(rebate, client, df):
     account_condition = df['account'].isin(rebate['accounts'])
     
     # Calculate the YTD volume for each combination of clientId, exchange, salesman_code, and account
-    df['ytd_volume'] = df.groupby(['clientId','exchange','account','salesman_code','year'])['volume'].transform('sum')
+    df['ytd_volume'] = df.groupby(['clientId','exchange','account','salesman_code','product_name','year'])['volume'].transform('sum')
     
     # Filter on active month global variable - we now have all the data that we need
     df = df[(df['month'] == month)]
@@ -127,22 +108,13 @@ def apply_yearly_rebate(rebate, client, df):
     df['eligibility'] = exchange_condition & salesman_condition & account_condition & volume_condition
 
     # Conditionally calculate 'volume_eligible'
-    df['volume_eligible'] = df.apply(lambda row: row['ytd_volume'] - rebate['threshold'] if row['eligibility'] else None, axis=1)
+    #df['volume_eligible'] = df.apply(lambda row: row['ytd_volume'] - rebate['threshold'] if row['eligibility'] else None, axis=1)
 
 
-    # Conditionally calculate the volume rebated and the actual amount
-    df[['volume_rebated', 'volume_rebated_amount']] = df.apply(lambda row: calculate_rebate(row, rebate.get("rebate_amount_per_contract")), axis=1)
+    # Conditionally calculate the volume eligible, rebated and the actual amount
+    df[['volume_eligible','volume_rebated', 'volume_rebated_amount']] = df.apply(lambda row: calculate_rebate(row, rebate, True), axis=1)
+
     return df
-
-# Function to calculate rebate
-def calculate_rebate(row, rebate_amount):
-    if row['eligibility']:
-        volume_rebated = min(row['volume_eligible'], row['volume'])
-        volume_rebated_amount = volume_rebated * rebate_amount
-        return pd.Series([volume_rebated, volume_rebated_amount])
-    else:
-        return pd.Series([None, None])
-
 
 #Question - when it is monthly, is it applying on the fully volume when the threshold is met or on transactions above the threshold???
 def apply_monthly_rebate(rebate, client, df):
@@ -171,10 +143,10 @@ def apply_monthly_rebate(rebate, client, df):
     df['eligibility'] = exchange_condition & salesman_condition & account_condition & volume_condition
     
     # Conditionally calculate 'volume_eligible'
-    df['volume_eligible'] = df.apply(lambda row: row['volume'] - rebate['threshold'] if row['eligibility'] else None, axis=1)
+    #df['volume_eligible'] = df.apply(lambda row: row['volume'] - rebate['threshold'] if row['eligibility'] else None, axis=1)
 
-    # Conditionally calculate the volume rebated and the actual amount
-    df[['volume_rebated', 'volume_rebated_amount']] = df.apply(lambda row: calculate_rebate(row, rebate.get("rebate_amount_per_contract")), axis=1)
+   # Conditionally calculate the volume eligible, rebated and the actual amount
+    df[['volume_eligible','volume_rebated', 'volume_rebated_amount']] = df.apply(lambda row: calculate_rebate(row, rebate, True), axis=1)
 
     return df
 
@@ -210,6 +182,25 @@ def apply_default_rebate(rebate, client, df):
 
     return df
 
+# Function to calculate rebate
+def calculate_rebate(row, rebate, fullRebate):
+    #when is volume eligible <> volume rebated ??
+
+    if row['eligibility']:
+        # if rebate impacts the full volume as soon as we hit the threshold
+        if fullRebate:
+            volume_eligible = row['volume']
+            volume_rebated = row['volume']
+        else:
+            # if rebate starts after the threshold is breached
+            volume_eligible = row['ytd_volume'] - rebate.get("threshold") if rebate.get("type") == "yearly" else row['volume'] - rebate.get("threshold")
+            volume_rebated = min(volume_eligible, row['volume'])
+        
+        # amount calculation
+        volume_rebated_amount = volume_rebated * rebate.get("rebate_amount_per_contract")
+        return pd.Series([volume_eligible, volume_rebated, volume_rebated_amount])
+    else:
+        return pd.Series([None, None, None])
 
 # Dictionary mapping rebate types to their corresponding functions
 rebate_functions = {
